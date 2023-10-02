353b2974d1e94c31a9efc17c6b96e906d968e73f
vickyone0 <vigneshs0.618@gmail.com>
2023-10-02 17:57:14 +0530

    added a function to find the habitable plants by the data given from the kepler

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const parse = require('csv-parse');
-
+const { parse } = require('csv-parse');
 const fs = require('fs');
 
+const habitablePlanets = [];
 
-const results = [];
-
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
-.pipe(parse({
-    Comment : '#',
+  .pipe(parse({
+    comment: '#',
     columns: true,
-}))
-.on('data',(data)=>{
-    results.push(data);
-})
-.on('error',(err)=>{
+  }))
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (err) => {
     console.log(err);
-})
-    .on('end',()=>{
-        console.log(results);
-        console.log('Done');
-    });
+  })
+  .on('end', () => {
+    console.log(habitablePlanets.map((planet) => {
+      return planet['kepler_name'];
+    }));
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+  });
